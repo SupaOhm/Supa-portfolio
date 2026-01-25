0196011fa28fa6a1861890d4deafe11919cf5d7c
ohm <supa@Supakorns-MacBook-Air.local>
2026-01-25 22:55:26 +0700

    edit animations and effects

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -39,8 +39,8 @@ export default function Connect() {
     {
       name: 'Phone',
       href: 'tel:+66895772122',
-      detail: '[+66] 89 577 2122',
-      description: 'Call me anytime',
+      detail: '(+66)\u00A089\u00A0577\u00A02122',
+      description: '',
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -51,7 +51,7 @@ export default function Connect() {
       name: 'Email',
       href: 'mailto:ohm.supakornth@gmail.com',
       detail: 'ohm.supakornth@gmail.com',
-      description: 'I reply within 24 hours',
+      description: '',
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -62,7 +62,7 @@ export default function Connect() {
       name: 'GitHub',
       href: 'https://github.com/SupaOhm',
       detail: '@SupaOhm',
-      description: 'Check out my projects',
+      description: '',
       icon: (
         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
           <path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z" clipRule="evenodd" />
@@ -73,7 +73,7 @@ export default function Connect() {
       name: 'LinkedIn',
       href: 'https://linkedin.com/in/supakornpra',
       detail: '/in/supakornpra',
-      description: 'Connect with me',
+      description: '',
       icon: (
         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
           <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
@@ -127,25 +127,25 @@ export default function Connect() {
                 {hoveredLink === link.name && (
                   <>
                     <div
-                      className="absolute w-[280px] h-[280px] bg-gradient-to-r from-blue-400/30 via-purple-400/25 to-transparent rounded-full blur-[80px] pointer-events-none opacity-100"
-                      style={{
-                        left: `${smoothMousePosition.x - 140}px`,
-                        top: `${smoothMousePosition.y - 140}px`,
-                        transition: 'none',
-                      }}
-                    />
-                    <div
-                      className="absolute w-[160px] h-[160px] bg-gradient-to-r from-blue-300/25 to-transparent rounded-full blur-[50px] pointer-events-none opacity-100"
+                      className="absolute w-[160px] h-[160px] bg-gradient-to-r from-blue-400/30 via-purple-400/25 to-transparent rounded-full blur-[50px] pointer-events-none opacity-100"
                       style={{
                         left: `${smoothMousePosition.x - 80}px`,
                         top: `${smoothMousePosition.y - 80}px`,
                         transition: 'none',
                       }}
                     />
+                    <div
+                      className="absolute w-[100px] h-[100px] bg-gradient-to-r from-blue-300/25 to-transparent rounded-full blur-[30px] pointer-events-none opacity-100"
+                      style={{
+                        left: `${smoothMousePosition.x - 50}px`,
+                        top: `${smoothMousePosition.y - 50}px`,
+                        transition: 'none',
+                      }}
+                    />
                   </>
                 )}
                 
-                <span className="relative z-10">
+                <span className={`relative z-10 transition-transform duration-300 ${hoveredLink === link.name ? 'scale-125' : 'scale-100'}`}>
                   {link.icon}
                 </span>
                 
